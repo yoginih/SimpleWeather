ee4b45693b3746e8cc93a1f77c3143e8f52dbbb3
yoginih <yogi@Yogis-MacBook-Pro.local>
2014-10-30 11:22:41 +0700

    Simple Weather
    Test aja

---
+++ SimpleWeather/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  SimpleWeather
+//
+//  Created by Yogi on 10/30/14.
+//  Copyright (c) 2014 SwitHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ SimpleWeather/BIDViewController.m
@@ -0,0 +1,38 @@
+//
+//  BIDViewController.m
+//  SimpleWeather
+//
+//  Created by Yogi on 10/30/14.
+//  Copyright (c) 2014 SwitHome. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
