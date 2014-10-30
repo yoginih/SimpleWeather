30779f2965511e4aeb7a82b2c7656edc083400d3
Yogi <yogi@Yogis-MacBook-Pro.local>
2014-10-30 10:13:13 +0700

    Initial Commit

---
+++ SimpleWeather/BIDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  BIDAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Yogi on 10/30/14.
+//  Copyright (c) 2014 SwitHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Yogi on 10/30/14.
+//  Copyright (c) 2014 SwitHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
